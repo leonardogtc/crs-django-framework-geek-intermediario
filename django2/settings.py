bdb1dd63c32fc2eb3cd9f44c9919024f078209d1
Leonardo G.T. da Conceição <leonardogtc@gmail.com>
2025-04-02 10:51:30 -0300

    002 - Criando arquivos de views e templates assim como diretório para arquivos statics

--- django2/settings.py
+++ django2/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = "django2.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": ["templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
