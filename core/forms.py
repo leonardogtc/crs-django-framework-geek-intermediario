d96e56c636311ab700ba2ea0f19f8de747f17d51
Leonardo G.T. da Conceição <leonardogtc@gmail.com>
2025-04-02 18:22:28 -0300

    Trabalhando com Formulários - Parte 1

---
+++ core/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+
+
+class ContatoForm(forms.Form):
+    nome = forms.CharField(label="Nome", max_length=100)
+    email = forms.EmailField(label="Email", max_length=200)
+    assunto = forms.CharField(label="Assunto", max_length=100)
+    mensagem = forms.CharField(label="Mensagem", widget=forms.Textarea())
--- core/views.py
+++ core/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from django.contrib import messages
+
+from core.forms import ContatoForm
 
 
 def index(request):
@@ -6,7 +9,36 @@ def index(request):
 
 
 def contato(request):
-    return render(request, "contato.html")
+    # Instancia o formulário informando que pode estar vazio ou ter recebido POST
+    form = ContatoForm(request.POST or None)
+
+    # Verifica se o formulário foi enviado e é válido
+    if request.method == "POST" and form.is_valid():
+        # Armazenar em variáveis
+        nome = form.cleaned_data["nome"]
+        email = form.cleaned_data["email"]
+        assunto = form.cleaned_data["assunto"]
+        mensagem = form.cleaned_data["mensagem"]
+
+        # Aqui você pode processar os dados do formulário
+        # Por exemplo, enviar um e-mail ou salvar em um banco de dados
+        print("Formulário enviado com sucesso!")
+        print("Nome:", {nome})
+        print("Email:", {email})
+        print("Assunto:", {assunto})
+        print("Mensagem:", {mensagem})
+
+        messages.success(request, "Mensagem enviada com sucesso!")
+        form = ContatoForm()  # Limpa o formulário após o envio
+
+    else:
+        messages.error(request, "Erro ao enviar a mensagem. Tente novamente.")
+
+    context = {
+        "form": form,
+    }
+
+    return render(request, "contato.html", context)
 
 
 def produto(request):
