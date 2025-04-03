7ca87116de67918b1d5ee6fc028c430fe099cd0c
Leonardo G.T. da Conceição <leonardogtc@gmail.com>
2025-04-03 11:27:32 -0300

    006 - Definindo e configurando models.

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import Produto
+
 
 # Register your models here.
+@admin.register(Produto)
+class ProdutoAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "nome",
+        "slug",
+        "preco",
+        "estoque",
+        "descricao",
+        "created_at",
+        "updated_at",
+        "ativo",
+    )
+    search_fields = ("nome",)
+    list_filter = ("nome",)
+    ordering = ("-id",)
+    list_per_page = 10
--- core/models.py
+++ core/models.py
@@ -1,3 +1,54 @@
 from django.db import models
+from stdimage.models import StdImageField
+
+# SIGNALS
+from django.db.models import signals
+from django.template.defaultfilters import slugify
+
 
 # Create your models here.
+class Base(models.Model):
+    """
+    Base model for all models in the app.
+    """
+
+    created_at = models.DateTimeField("Data de Criação", auto_now_add=True)
+    updated_at = models.DateTimeField("Data de atualização", auto_now=True)
+    ativo = models.BooleanField("Ativo", default=True)
+
+    class Meta:
+        abstract = True
+
+
+class Produto(Base):
+    """
+    Model for products.
+    """
+
+    nome = models.CharField("Nome", max_length=255)
+    preco = models.DecimalField("Preço", max_digits=10, decimal_places=2, default=0.00)
+    estoque = models.IntegerField("Estoque", default=0)
+    descricao = models.TextField("Descrição")
+    imagem = StdImageField(
+        "Imagem",
+        upload_to="produtos",
+        variations={"thumb": {"width": 300, "height": 300, "crop": "smart"}},
+    )
+    slug = models.SlugField(
+        "Slug", max_length=255, unique=True, blank=True, editable=False
+    )
+
+    def __str__(self):
+        return self.nome
+
+
+def produto_pre_save(signal, instance, **kwargs):
+    """
+    Pre-save signal for Produto model.
+    """
+    if not instance.slug:
+        instance.slug = slugify(instance.nome)
+    instance.slug = slugify(instance.nome)
+
+
+signals.pre_save.connect(produto_pre_save, sender=Produto)
